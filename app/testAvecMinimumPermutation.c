9a00d5d583bfd6fa0d0e5468672971a81fc05054
FArcellier <noreply@fabien-tools.net>
2011-04-10 12:50:05 +0000

    Import des codes sur lesquels on a travaillé le 07/04/2011

---
+++ app/testAvecMinimumPermutation.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <math.h>
+
+#define NOMBRE_DIGIT_MAXIMUM 10
+#define RANGE_DEBUT 0
+#define RANGE_FIN 2000
+
+/* Liste des prototypes
+*/
+long ObtenirNombreArmstrong (int combinaison[], int ordre_courant, long debut, long fin);
+
+/*
+
+*/
+int main(void)
+{
+	long debut = RANGE_DEBUT;
+	long fin = RANGE_FIN;
+	int combinaison[NOMBRE_DIGIT_MAXIMUM];
+	int i = 0;
+	
+
+	int ordre_debut = (int) log10(debut);
+        int ordre_fin = ((int) log10(fin)) + 1;
+	int ordre_courant = ordre_debut;
+
+	for(i = 0; i < NOMBRE_DIGIT_MAXIMUM; i++)
+	{
+		combinaison[i] = 0;
+	}
+
+	do
+	{
+		long nombreArmstrong = ObtenirNombreArmstrong(combinaison, 
+			ordre_courant, 
+			debut, 
+			fin);
+
+		if (nombreArmstrong != -1)
+		{
+			printf("%ld\n", nombreArmstrong);
+		}
+
+		ObtenirCombinaisonSuivante(combinaison, &ordre_courant);
+	} while (ordre_courant < ordre_fin);
+
+	return 0;
+}
+
+long ObtenirNombreArmstrong (int combinaison[], int ordre_courant, long debut, long fin)
+{
+	long valeurArmstrong = 0;
+	int resultat = -1;// Aucun resultat
+	int i = 0;
+	for(i = 0; i < ordre_courant; i++)
+	{
+		valeurArmstrong = valeurArmstrong + 
+			pow(combinaison[i], ordre_courant);
+	}
+
+	// int nombre_permutation = fact(ordre_courant);
+	for(i = 0; i < nombre_permutation; i++)
+	{
+		long valeurCombinaison = ObtenirPermutation(
+			combinaison, 
+			ordre_courant,
+			i);
+
+		if (valeurCombinaison == valeurArmstrong)
+		{
+			resultat = valeurArmstrong;
+			break;
+		}
+	}
+
+	return resultat;
+}
+
+long ObtenirPermutation(int combinaison[], int ordre_courant, int i)
+{
+	
+}
---
+++ app/testBourrin.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <sys/time.h>
+#define LONGUEUR_DATA 10
+
+void IncrementData (int data[]);
+int RecherchePremierNonNul(int data[]);
+long CalculArmstrong(int data[], int n);
+
+int main(void)
+{
+	long time_start = time(NULL);
+	long time_end = 0;
+	int data[LONGUEUR_DATA];
+	int i = 0;
+	for (i = 0; i < LONGUEUR_DATA; i++)
+	{
+		data[i]=0;
+	}
+
+	long end = 2000000;
+	long k = 1;
+	for (k = 1; k < end; k++)
+	{
+		IncrementData(data);
+		int n = RecherchePremierNonNul(data);
+		long Armstrong = CalculArmstrong(data, n);
+		// printf("debug Armstrong %d - k %d\n", Armstrong, k);
+		if (Armstrong == k)
+		{
+			printf("%ld\n", Armstrong);
+		}
+	}
+	time_end = time(NULL);
+	printf("Temps (s) : %ld\n", time_end - time_start);
+
+	return 0;
+}
+
+void IncrementData (int data[])
+{
+	int i = 0;
+	data[LONGUEUR_DATA - 1]++;
+	// printf("Debug Tableau ");
+	for (i = LONGUEUR_DATA - 1; i >= 0; i--)
+	{
+		// printf("%d",data[i]);
+		if (data[i] == 10)
+		{
+			data[i] = 0;
+			data[i - 1]++;
+		}
+	}
+	// printf("\n");
+
+	return;
+}
+
+int RecherchePremierNonNul(int data[])
+{
+	int i = 0;
+	for(i = 0; i < LONGUEUR_DATA; i++)
+	{
+		if (data[i] != 0)
+		{
+			break;
+		}
+	}
+	// printf("debug : %d\n", i);
+	return LONGUEUR_DATA - i;
+}
+
+long CalculArmstrong(int data[], int n)
+{
+	int i = 0;
+	long result = 0;
+	for (i = 0; i < n; i++)
+	{
+		result = result + pow(data[LONGUEUR_DATA -1 -i], n);
+	}
+	// printf("debug Armstrong :  %d\n", result);
+
+	return result;
+}
