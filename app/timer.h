0d72529ecdbc3afcf5ee6199d939b2ad31230012
rimaxime <noreply@fabien-tools.net>
2011-04-12 17:42:36 +0000

    [Fonctions Timer] 	long long timer_start(void) 	long long timer_end(long long time_depart) Exemple d'utilisation dans le testNbArmstrong.c qui m'a servi de plateforme pour le test

---
+++ app/timer.h
@@ -0,0 +1,26 @@
+/*! \file timer.h
+	\author Maxime RIVIERE
+*/
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <sys/time.h>
+#include <time.h>
+
+/*! \brief Fonctions de calculs du temps d'exécution du programme
+
+  Vous devez éviter de manipuler directement une instance de ce type la.
+  Utilisez les fonctions ci-dessous.
+*/
+
+
+/*! \brief Renvoi le temps initial d'exécution du programme
+*/
+long long time_start(void);
+
+/*! \brief Retourne le temps total d'exécution du programme
+*/
+long long time_end(long long time_depart);
+
+
+
