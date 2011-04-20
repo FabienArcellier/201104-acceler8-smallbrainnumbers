4cba082b6dca0c690ed2f81a3e3656e8019d346c
FArcellier <noreply@fabien-tools.net>
2011-04-20 12:13:17 +0000

    Modification de l'ordonnanceur suite aux échanges de mails hier soir et ce matin. Il va falloir retirer toute référence à la fabrique. On verra ça une fois qu'on aura une première version fonctionnelle.
    Il manque la fonction GetOrdreDepartCombinaison à programmer.
    - prototype int GetOrdreDepartCombinaison(char * combinaison, int size);
    - parcourt la combinaison depuis le poids fort tant qu'elle rencontre des 0. dès qu'elle rencontre un nombre différent, elle renvoie sa size - sa position

--- app/ordonnanceur.c
+++ app/ordonnanceur.c
@@ -15,27 +15,46 @@ Ordonnanceur inline * Ordonnanceur_Init(int nombre_processeurs, FabriqueTacheCom
 void Ordonnanceur_Start(Ordonnanceur *ordonnanceur)
 {
   int reste_des_taches = 1;
-  long long i;
+  int ordre_finale = ordonnanceur -> fabrique -> ordre_finale;
+  long long i = 0;
   int nb_combinaison_max;
   int nombre_proc = ObtenirNombreProcesseurs();
-  nb_combinaison_max = nombreCombinaisonsDunRange(ordonnanceur->fabrique->ordre_initiale, ordonnanceur->fabrique->ordre_finale);  
-  #pragma omp parallel for private(reste_des_taches) num_threads(nombre_proc*2)
-  for(i=0;i<nb_combinaison_max;i++)
+  nb_combinaison_max = nombreCombinaisonsDunNombreDigit(ordre_finale);
+  
+  // Réservation de l'espace mémoire pour les permutations et construction de la combinaison initiale
+  char * permutations = (char *) malloc(sizeof(char) * ordre_finale * nb_combinaison_max);
+  char * combinaison = (char *) malloc(sizeof(char) * ordre_finale);
+  for (i = 0; i < ordre_finale; i++)
   {
-    //printf("D: Nombre de Thread : %d\n",omp_get_num_threads());
-	  if(reste_des_taches ==1)
-	  {
-	    TacheCombinaisonEstNombreArmstrong *tache = NULL;
-	    #pragma omp critical
-	    {
-	    reste_des_taches = FabriqueTacheCombinaisonEstNombreArmstrong_ObtenirTacheSuivante(ordonnanceur -> fabrique, &tache);
-	    }
-	    if (reste_des_taches == 1)
-	    {
-		    TacheCombinaisonEstNombreArmstrong_Executer(tache);
-		    TacheCombinaisonEstNombreArmstrong_Detruire(tache);
-	    }
-  } 	
+    combinaison[i] = 0;
+  }
+  
+  for (i = 0; i < nb_combinaison_max; i++)
+  {
+    IncrementerTableauxDigitCombinaisonUnique(combinaison, ordre_finale);
+    memcpy(permutation[i], combinaison, sizeof(char) * ordre_finale);
+  }
+  
+  // Execution du traitement parallèle pour trouver les nombres d'armstrong
+  for (i = 0; i < nb_combinaison_max; i++)
+  {
+    int j = 0;
+    // int ordre_min = GetOrdreDepartCombinaison(permutation[i]);
+    for (j = ordre_min; j <= ordre_finale; j++)
+    {
+      long long valeurArmstrong = CalculNombreArmstrong(permutation[i], j);
+      if (nombre > ordonnanceur -> fabrique -> valeur_finale &
+        nombre < ordonnanceur -> fabrique -> valeur_initiale)
+      {
+        continue;
+      }
+      
+      if (EstUnNombreArmstrong(permutation[i], nombre, j, ordonnanceur -> fabrique -> cachePuissance10))
+      {
+        printf("NA: %lld\n", nombre);
+      }
+    }
+  }
 }
 
 /*! \brief Permet a un code exterieur de se mettre en attente de la fin de l'ordonnanceur
